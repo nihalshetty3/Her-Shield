aa2109a39ea5dec54f51a42aa0b9b985aff40ddb
Nihal Shetty <nihalhshetty30@gmail.com>
2026-07-05 19:52:22 +0530

    feat: Updated notification with call service

---
+++ backend/src/services/call.service.js
@@ -0,0 +1,33 @@
+const client = require("../integrations/twilio.client");
+
+const makeEmergencyCall = async(incident) => {
+
+    try{
+        const call = await client.calls.create({
+            to:process.env.GUARDIAN_PHONE,
+            from:process.env.TWILIO_PHONE_NUMBER,
+
+            twiml:`
+            <Response>
+            <Say voice="Nihal">
+             Emergency Alert from Her Shield.
+            An SOS has been triggered.
+            Please contact the user immediately.
+            </Say>
+            </Response>
+            `
+        });
+
+        console.log("Call Initiated");
+        console.log("Call SID:" , call.sid);
+    }
+
+    catch(err){
+        console.log("Call Failed");
+        console.log(err.message);
+    }
+};
+
+module.exports = {
+    makeEmergencyCall
+};
