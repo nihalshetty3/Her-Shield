58c12e1d8920d1ad391966bc93e0c89504879fc3
Nihal Shetty <nihalhshetty30@gmail.com>
2026-07-05 19:52:22 +0530

    feat: Updated notification with call service

--- backend/src/services/notification.service.js
+++ backend/src/services/notification.service.js
@@ -1,10 +1,12 @@
 const SMSService = require("./sms.service");
-
+const CallService = require("./call.service");
 const sendEmergencyAlert = async (incident) => {
 
     console.log("\n========== NOTIFICATION SERVICE ==========");
 
     await SMSService.sendSMS(incident);
+    
+    await CallService.makeEmergencyCall(incident);
 
     console.log("==========================================\n");
 
